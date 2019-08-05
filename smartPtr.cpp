238e168b50d9e2ecdfbc35aa7579e0cf40353772
吴师顺 <wushishun@bimwinner.com>
2019-08-05 19:38:27 +0800

    operate sqlite3

--- smartPtr.cpp
+++ smartPtr.cpp
@@ -3,6 +3,24 @@
 #include <memory>
 using namespace std;
 
+shared_ptr<string> factory(const char* p){
+    return make_shared<string>(p);
+}
+
+void use_factory(){
+    shared_ptr<string> p = factory("helloworld");
+	cout<<p.use_count()<<endl;	
+	cout<<*p<<endl;			// 离开作用域时，p引用的对象被销毁。
+
+} 
+shared_ptr<string> return_share_ptr()
+{ 
+	shared_ptr<string> p = factory("helloworld"); 
+	cout<<*p<<endl; 
+	return p;							   // 返回p时，引用计数进行了递增操作。 
+}											// p离开了作用域，但他指向的内存不会被释放掉。 
+
+
 int main(int argc, char* argv[])
 {
 	std::shared_ptr<int> sp0(new int(2));
@@ -48,6 +66,11 @@ int main(int argc, char* argv[])
 	printf("%d\n", *sp5);               // 33
 	printf("%d\n", *sp6);               // 22
 	printf("%d\n", *sp7);               // 22
+	
+	std::cout<<"============="<<std::endl;
+	use_factory();
+	auto p = return_share_ptr();
+	cout<<p.use_count()<<endl;
 	
 	return 0;
 } 
