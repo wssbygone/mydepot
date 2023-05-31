0430a1b832b9f17e5af0277a6da3a03988bdcfe3
吴师顺 <wushishun@minieye.cc>
2023-05-31 12:24:14 +0800

    update

--- mini/xx.cpp
+++ mini/xx.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
-#include <string>
-#include<memory> 
  
-struct boot 
-{ 
-    boot() { printf("Boot\n"); } 
-    ~boot() { printf("Unboot\n"); } 
-}; 
- 
-boot instance = {}; 
- 
-int main() 
+
+int main()
 {
-  int i=2;
-  std::shared_ptr<int> p(new int(2));
-  return 0;
-} 
+    int i = 100;//局部变量
+    //将i拷贝到lambda表达式f的可调用对象，也就是把i是100这个值传到了f中，f中用的i一直就是100，不会改变。
+    auto f = [i] { return i; };
+    i = 0;
+    int j = f(); //j=100,因为i是f创建时拷贝的，而不是运行时，所以即使后面i=0也不起作用。
+    printf("j = %d\n",j);
+    return 0;
+}
