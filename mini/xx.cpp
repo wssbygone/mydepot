544a432b51412db4f0a2c7a64c9d8d6b1d3170a0
吴师顺 <wushishun@minieye.cc>
2023-07-28 20:08:25 +0800

    update

--- mini/xx.cpp
+++ mini/xx.cpp
@@ -2,20 +2,23 @@
 #include <bitset>
 #include <iostream>
 #include <string>
-
+#include <typeinfo>
 #include <cstdio>
-class SS {
-//  char* bufferp;
-//  size_t length;
-  union {
-    char local_buffer[16];
-    size_t capacity;
-  };
+struct SS {
+  SS(int a=98):a_(a) { }
+  int a_;
 };
 
 int main () {
+  SS *s;
+  decltype(s) pB;
+  pB=nullptr;
+  std::cout<<typeid(pB).name()<<std::endl;    
+  decltype(s) pC=new SS(77);
+  std::cout<<pC->a_<<std::endl;    
 
-  std::cout<<sizeof(SS)<<std::endl;    
+  const std::string &topicName="abcd";
+  std::cout<<topicName<<std::endl;
   return 0;
 }
  
--- shell/ping_all_active_ip.sh
+++ shell/ping_all_active_ip.sh
@@ -1,5 +1,5 @@
 cat /dev/null > xx.log
-for line in $(seq 98 107) 
+for line in $(seq 10 246) 
 do
   echo "$line"
 #  if [[ -z `grep Unreachable  $(ping -c 1 192.168.66.${line})` ]];then
@@ -10,4 +10,5 @@ done
 echo "~~~~~~~~~~~~~~~~~~"
 
 grep "64 bytes from" xx.log|grep -v  Unreachable |awk '{print $4}' |tr -d ":"|sort -t . -n -k 4 |uniq 
+rm -rf xx.log
 
