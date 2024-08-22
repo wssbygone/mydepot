cb6d118cbb95ebbae751351398c05ec193f22344
吴师顺 <wushishun@minieye.cc>
2024-08-22 16:15:07 +0800

    bash exercise

---
+++ mini/shell/formatStr_arrayDelItem.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mdl=("module1" "asan" "teacher" "jin")
+accumulate_str=""
+for it in "${mdl[@]}"
+do 
+  formatted_string="$(printf "%-15s %s" "$it" "$(git rev-parse --short HEAD)\n")"
+  echo -e "$formatted_string"
+  echo "-----------"
+  accumulate_str+="$formatted_string"
+  echo -e "$accumulate_str"
+done
+
+for it in "${mdl[@]}"
+do
+   length=${#mdl[@]}
+#   unset mdl[length-1]   #虽然不报错，但是下行写法更严谨
+   unset mdl[$((length-1))]  
+   echo ${#mdl[@]}
+done
